24ae4203d05d9f9e65fcd4f8576d4ffc1693522d
Sagi Barshai <sagibarshai1@gmail.com>
2025-07-16 19:39:15 +0300

    remove all category

--- src/pages/glass.tsx
+++ src/pages/glass.tsx
@@ -3,7 +3,7 @@ import Gallery from "../components/gallery";
 import { asset } from "../utils/asset";
 
 const Glasses: React.FC = () => {
-  const imageList1 = Array.from({ length: 6 }, (_, i) => ({
+  const imageList1 = Array.from({ length: 13 }, (_, i) => ({
     src: asset(`/images/category-3/${i + 1}.jpg`),
     alt: `תמונה ${i + 1} מתוך קטגוריה 1`,
   }));
