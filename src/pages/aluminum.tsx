b62e146e2345756dd458a6adf09b4936e2a1556f
Sagi Barshai <sagibarshai1@gmail.com>
2025-07-16 19:04:37 +0300

    remove all category

--- src/pages/aluminum.tsx
+++ src/pages/aluminum.tsx
@@ -3,8 +3,8 @@ import Gallery from "../components/gallery";
 import { asset } from "../utils/asset";
 
 const Aluminum: React.FC = () => {
-  const imageList1 = Array.from({ length: 17 }, (_, i) => ({
-    src: asset(`/images/category-1/${i + 1}.jpg`),
+  const imageList1 = Array.from({ length: 30 }, (_, i) => ({
+    src: asset(`/images/category-2/${i + 1}.jpg`),
     alt: `תמונה ${i + 1} מתוך קטגוריה 1`,
   }));
 
