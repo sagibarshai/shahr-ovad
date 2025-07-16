72d3081762f53e942980ef15787e8133b94fb48e
Sagi Barshai <sagibarshai1@gmail.com>
2025-07-16 19:34:51 +0300

    remove all category

--- src/pages/showers.tsx
+++ src/pages/showers.tsx
@@ -3,8 +3,8 @@ import Gallery from "../components/gallery";
 import { asset } from "../utils/asset";
 
 const Showers: React.FC = () => {
-  const imageList1 = Array.from({ length: 5 }, (_, i) => ({
-    src: asset(`/images/category-3/${i + 1}.jpg`),
+  const imageList1 = Array.from({ length: 17 }, (_, i) => ({
+    src: asset(`/images/category-1/${i + 1}.jpg`),
     alt: `תמונה ${i + 1} מתוך קטגוריה 1`,
   }));
   return <Gallery images={imageList1} />;
